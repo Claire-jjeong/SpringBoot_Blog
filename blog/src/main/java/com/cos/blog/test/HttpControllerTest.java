9c3094aa342c97ad2018ebde3202c62cd03d9972
Claire-jjeong <kjjung7026@gmail.com>
2022-01-30 00:18:30 +0900

    lombok 실습

--- blog/src/main/java/com/cos/blog/test/HttpControllerTest.java
+++ blog/src/main/java/com/cos/blog/test/HttpControllerTest.java
@@ -15,10 +15,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HttpControllerTest {
 
+	private static final String TAG = "HttpControllerTest : ";
+	
+	@GetMapping("/http/lombok")
+	public String lombokTest() {
+		Member m = Member.builder().username("ssar").password("1234").email("ssar@nate.com").build();
+		System.out.println(TAG+"getter : " + m.getUsername());
+		m.setUsername("cos");
+		System.out.println(TAG+"setter : " + m.getUsername());
+		return "lombok test 완료";
+	}
+	
 	//인터넷 브라우저 요청은 무조건 get요청밖에 할 수 없다. 
 	//http://localhost:8080/http/get (select)
 	@GetMapping("/http/get")
 	public String getTest(Member m) {
+		
 		return "get 요청 : " +m.getId() + ", " + m.getUsername()+", "+m.getPassword()+", "+m.getEmail(); 
 	}
 	//http://localhost:8080/http/post(insert)
