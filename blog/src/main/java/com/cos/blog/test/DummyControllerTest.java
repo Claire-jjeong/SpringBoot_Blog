05c457244e407ce038ff62a28f4c04db836c6336
Claire-jjeong <kjjung7026@gmail.com>
2022-02-03 15:00:20 +0900

    Paging Test

--- blog/src/main/java/com/cos/blog/test/DummyControllerTest.java
+++ blog/src/main/java/com/cos/blog/test/DummyControllerTest.java
@@ -1,8 +1,13 @@
 package com.cos.blog.test;
 
+import java.util.List;
 import java.util.function.Supplier;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +22,21 @@ public class DummyControllerTest {
 
 	@Autowired //의존성 주입 
 	private UserRepository userRepository;
+	
+	
+	@GetMapping("/dummy/users")
+	public List<User>list(){
+		return userRepository.findAll();
+	}
+	
+	//한 페이지당 2건의 데이터를 리턴받아 볼 예정 
+	@GetMapping("dummy/user")								//한 페이지 당 2건이기 때문에 사이즈 2
+	public List<User>pageList(@PageableDefault(size=2, sort="id",direction=Sort.Direction.DESC)Pageable pageable){
+		Page<User> pagingUser = userRepository.findAll(pageable);
+		
+		List<User> users = pagingUser.getContent();
+		return users;
+	}
 	
 	//{id}주소로 파라미터를 전달 받을 수 있음
 	//http://localhost:8000/blog/dummy/user/3
