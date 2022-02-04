026115bd8a876e43d637bb9c0dfddb4c05497aac
Claire-jjeong <kjjung7026@gmail.com>
2022-02-04 20:23:50 +0900

    update test

--- blog/src/main/java/com/cos/blog/test/DummyControllerTest.java
+++ blog/src/main/java/com/cos/blog/test/DummyControllerTest.java
@@ -3,6 +3,8 @@ package com.cos.blog.test;
 import java.util.List;
 import java.util.function.Supplier;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -11,6 +13,8 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cos.blog.model.RoleType;
@@ -22,7 +26,28 @@ public class DummyControllerTest {
 
 	@Autowired //의존성 주입 
 	private UserRepository userRepository;
-	
+	//save함수는 id를 전달하지 않으면 insert를 해주고
+	//save 함수는 id를 전달하면 해당 id에 대한 데이터가 있으면 update를 해주고
+	//save 함수는 id를 전달하고 해당 id에 대한 데이더가 없으면 insert를 함 
+	//email,password 수정 
+	@Transactional
+	@PutMapping("/dummy/user/{id}")						//json데이터 받아오기(requestBody)
+	public User updateUser(@PathVariable int id, @RequestBody User requestUser) { //json데이터 요청 => java object(Message Converter의 Jackson라이브러리가 변환해서 받아줌) 
+		System.out.println("id : "+id);
+		System.out.println("password : "+requestUser.getPassword());
+		System.out.println("email: "+requestUser.getEmail());
+		
+		User user = userRepository.findById(id).orElseThrow(()->{
+			return new IllegalArgumentException("수정에 실패하였습니다.");
+		}); //아이디를 찾고 찾지 못하면 
+		user.setPassword(requestUser.getPassword());
+		user.setEmail(requestUser.getEmail());
+		
+		//userRepository.save(user); 
+		//Transactional 넣어주고 save를 하지 않았는데 업데이트 됨->
+		//더티 체킹 
+		return null;
+	}
 	
 	@GetMapping("/dummy/users")
 	public List<User>list(){
