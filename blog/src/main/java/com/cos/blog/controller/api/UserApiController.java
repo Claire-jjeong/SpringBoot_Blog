d9a9d3e9d9621995e1b21ba6e94ef61fed9ad665
Claire-jjeong <kjjung7026@gmail.com>
2022-02-14 00:53:14 +0900

    제이쿼리 script추가

--- blog/src/main/java/com/cos/blog/controller/api/UserApiController.java
+++ blog/src/main/java/com/cos/blog/controller/api/UserApiController.java
@@ -6,12 +6,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.cos.blog.model.User;
 
+import dto.ResponseDto;
+
 @RestController //데이터만 리턴 
 public class UserApiController {
 
 	@PostMapping("/api/user")
-	public int save(@RequestBody User user) { //요청 받는게 json이기 때문에 RequestBody선언 
+	public ResponseDto<Integer> save(@RequestBody User user) { //요청 받는게 json이기 때문에 RequestBody선언 
 		System.out.println("UserApiController : save 호출 됨 ");
-		return 1;
+		return new ResponseDto<Integer>(200,1);
 	}
 }
--- blog/src/main/resources/static/js/user.js
+++ blog/src/main/resources/static/js/user.js
@@ -1,41 +1,35 @@
 let index = {
-	let  _this = this;
-	init:function(){
-		//JQuery
-		$("#btn-save").on("click", function(){  //this를 바인딩 하기 위해서 사용
-			_this.save();
-		}); 
+	init: function(){
+		$("#btn-save").on("click", ()=>{
+			this.save();
+		});
 	},
 	
-	save:function(){
-		//alert("user의 save 함수 호출됨 ");
+	save : function(){
 		let data = {
-			//이 값들을 찾아서 data object에 넣어줌 
-			username: $("#username").val(),
-			password: $("#password").val(),
+			username : $("#username").val(),
+			password : $("#password").val(),
 			email: $("#email").val()
 		}
-		//값 잘 들고 왔는지 확인
-		//console.log(data);
 		
-		//ajax호출 시 default가 비동기 호출 
-		//ajax 통신을 이용해서 3개의 데이터를 json으로 변경하여 insert요청
+		//ajax호출시 default가 비동기 호출 
+		//ajax 통신을 이요해서 3개의 데이터를 json으로 변경하여 insert 요청!
 		$.ajax({
 			//회원가입 수행 요청 
-			type:"POST",
-			url:"/blog/api/user",
-			data: JSON.stringify(data), //data를 json형태로 바꿔주기 
-			contentType:"application/json; charset=utf-8", //body데이터가 어떤 타입인지(MIME)
-			dataType: "json" //요청을 서버로해서 응답이 왔을 때 기본적으로 모든 것이 문자열 (생긴게 json이라면) => javascript오브젝트로 변
-		}).done(function(resp){ //응답의 결과가 
-			//성공시 
+			type : "POST",
+			url: "/blog/api/user",
+			data: JSON.stringify(data),
+			contentType:"application/json; charset=utf-8", //body 데이터가 어떤 타입인지 
+			dataType: "json" //응답이 왔을 때 문자열 (생긴게 json이라면) => javascript 
+		}).done(function(resp){
 			alert("회원가입이 완료되었습니다.");
-			location.href = "/blog";
+			alert(resp);
+			location.href="/blog";
 		}).fail(function(error){
-			//실패시 
 			alert(JSON.stringify(error));
-		}); 
-		
+		});
 	}
+	
 }
+
 index.init();
