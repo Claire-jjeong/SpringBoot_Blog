57ae19423947d633de471d7c1e32ce13e72e029c
Claire-jjeong <kjjung7026@gmail.com>
2022-02-14 22:01:44 +0900

    service 오류 발견

--- blog/src/main/java/service/UserService.java
+++ blog/src/main/java/service/UserService.java
@@ -1,5 +1,7 @@
 package service;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -8,13 +10,14 @@ import com.cos.blog.repository.UserRepository;
 
 //스프링이 컴포넌트 스캔을 통해서 Bean에 등록을 해줌. IoC
 @Service
-//1. 트랜잭션 관리
+//1. 트랜잭션 관리 : 두 개의 트랜잭션을 하나로 묶어서 처리할 수 있음
 //2. 서비스 의미때문에 
 public class UserService {
 
 	@Autowired
 	private UserRepository userRepository;
 	
+	@Transactional //하나의 트랜잭션으로 묶임 ,성공하면 commit, 실패하면 rollback 
 	public int 회원가입(User user) {
 		try {
 			userRepository.save(user);
