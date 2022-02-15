6bdf096c5ab23e9cfae1cadddddbedb262eb07dd
Claire-jjeong <kjjung7026@gmail.com>
2022-02-15 22:49:50 +0900

    UserDto 수정

--- blog/src/main/java/com/cos/blog/handler/GlobalExceptionHandler.java
+++ blog/src/main/java/com/cos/blog/handler/GlobalExceptionHandler.java
@@ -1,8 +1,11 @@
 package com.cos.blog.handler;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
+
+import dto.ResponseDto;
 
 @ControllerAdvice //어떤 exception이 들어오더라도 여기에서 실행 
 @RestController
@@ -10,7 +13,7 @@ public class GlobalExceptionHandler {
 
 	//어떤 익셉션만 받을거냐?
 	@ExceptionHandler(value=Exception.class)
-	public String handleArgumentException(Exception e) {
-		return "<h1>"+e.getMessage()+"</h1>";
+	public ResponseDto<String> handleArgumentException(Exception e) {
+		return new ResponseDto<String>(HttpStatus.INTERNAL_SERVER_ERROR.value(),e.getMessage());
 	}
 }
